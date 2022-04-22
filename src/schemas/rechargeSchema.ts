f0a6fc132d128cf924738e506cc08e9a3b28cd02
Eduardo Vedooto <vedootoeduardo@gmail.com>
2022-04-22 11:05:50 -0300

    Preparando revisão de código

--- src/schemas/rechargeSchema.ts
+++
@@ -1,8 +0,0 @@
-import joi from "joi";
-
-const rechargeSchema = joi.object({
-    amount: joi.number().required(),
-    cardId: joi.number().required(),
-});
-
-export default rechargeSchema;
--- src/services/companyService.ts
+++
@@ -1,8 +0,0 @@
-import * as companyRepository from '../repositories/companyRepository.js';
-
-
-export async function validateApiKey(apiKey: string) {
-    const company = await companyRepository.findByApiKey(apiKey);
-    if(!company)
-            throw {message: 'Invalid ApiKey'}
-}
